beef6434894c710b508a6e031b045beba49ac42d
Scallion <xx@1234>
2023-03-17 16:04:36 +0800

    first main commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "test.h"
+
+
+
+int main() {
+    double a = 0,b = 0;
+    scanf ("%lf*f,%lf",&a,&b);
+    printf("%f %f",a,b);
+}
